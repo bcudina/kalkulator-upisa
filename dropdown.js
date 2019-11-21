ad7f12dd9480e85c0094d37b7a1fd6a096f73597
bcudina <bozidar.cudina@gmail.com>
2019-11-21 12:36:50 +0100

    stabilno 21112019

--- dropdown.js
+++ dropdown.js
@@ -1,64 +1,215 @@
-let tehnicar = "<b>Elektrotehničar</b> - min broj bodova <b>61,13</b><br/><b>Tehničar za elektroniku</b> - min broj bodova <b>68,16</b> <br/><b>Tehničar za računarstvo</b> - min.broj bodova <b>71,32</b>";
+let elektroJS =
+  "<b><mark>Elektrotehička škola</mark></b><br/><b>Elektrotehničar</b> - min broj bodova <b>61,13</b><br/><b>Tehničar za elektroniku</b> - min broj bodova <b>68,16</b> <br/><b>Tehničar za računarstvo</b> - min.broj bodova <b>71,32</b>";
 
-let strojarstvo = "<b>Strojarsko računalni tehbičar</b> - min broj bodova <b>51,16</b><br/><b>Tehničar za mehanotehniku</b> - min broj bodova <b>66,47</b>";
+let strojarstvo =
+  "<b><mark>Tehničar za strojarstvo i mehanotehniku</mark></b><br/><b>Strojarsko računalni tehbičar</b> - min broj bodova <b>51,16</b><br/><b>Tehničar za mehanotehniku</b> - min broj bodova <b>66,47</b>";
 
-let gimnazija1 = "<b>Jezična</b> - min broj bodova <b>71,56</b><br/><b>Klasična</b> - min broj bodova <b>62,61</b>";
+let gimnazija1 =
+  "<b><mark>I Gimnazija</mark></b><br/><b>Jezična</b> - min broj bodova <b>71,56</b><br/><b>Klasična</b> - min broj bodova <b>62,61</b>";
 
-let gimnazija2 = "<b>Jezična</b> - min broj bodova <b>69,21</b>";
+let gimnazija2 = "<b><mark>II Gimnazija</mark></b><br/><b>Jezična</b> - min broj bodova <b>69,21</b>";
 
-let gimnazija3 = "<b>Prirodoslovno-matematička</b> - min broj bodova <b>75,07</b>";
-let gimnazija4 = "<b>Opća gimnazija</b> - min broj bodova <b>71,86</b>";
-let gimnazija5 = "<b>Opća gimnazija</b> - min broj bodova <b>76,33</b><br/><b>Opća gimnazija (odjel za sportaše)</b> - min broj bodova <b>127,06</b>";
+let gimnazija3 =
+  "<b><mark>III Gimnazija</mark></b><br/><b>Prirodoslovno-matematička</b> - min broj bodova <b>75,07</b>";
+let gimnazija4 = "<b><mark>IV Gimnazija 'Marko Marulić'</mark></b><br/><b>Opća gimnazija</b> - min broj bodova <b>71,86</b>";
+let gimnazija5 =
+  "<b><mark>V Gimnazija 'Vladimir Nazor'</mark></b><br/><b>Opća gimnazija</b> - min broj bodova <b>76,33</b><br/><b>Opća gimnazija (odjel za sportaše)</b> - min broj bodova <b>127,06</b>";
 
-let ekonomskoBirotehnickaJS = "<b>Ekonomist-odjel za sportase</b> - min broj bodova <b>94,51</b><br/><b>Ekonomist</b> - min broj bodova <b>47,40</b><br/><b>Poslovni tajnik</b> - min broj bodova <b>48,11</b><br/><b>Upravni referent</b> - min broj bodova <b>54,05</b><br/>";
+let ekonomskoBirotehnickaJS =
+  "<b><mark>Ekonomsko Birotehnička škola</mark></b><br/><b>Ekonomist-odjel za sportaše</b> - min broj bodova <b>94,51</b><br/><b>Ekonomist</b> - min broj bodova <b>47,40</b><br/><b>Poslovni tajnik</b> - min broj bodova <b>48,11</b><br/><b>Upravni referent</b> - min broj bodova <b>54,05</b><br/>";
 
-let zdravstvenaJS = "<b>Medicinska sestra opće njege</b> - min broj bodova <b>63,63</b><br/><b>Fermaceutski tehničar</b> - min broj bodova <b>72,47</b><br/><b>Zdravstveno-laboratoriski tehničar</b> - min broj bodova <b>63,34</b><br/><b>Fizioterapeutski tehničar</b> - min broj bodova <b>68,11</b><br/><b>Dentalni asistent</b> - min broj bodova <b>63,79</b><br/><br/><b>Sanitarni tehničar</b> - min broj bodova <b>59,45</b>";
+let zdravstvenaJS =
+  "<b><mark>Zdravstvena škola</mark></b><br/><b>Medicinska sestra opće njege</b> - min broj bodova <b>63,63</b><br/><b>Fermaceutski tehničar</b> - min broj bodova <b>72,47</b><br/><b>Zdravstveno-laboratoriski tehničar</b> - min broj bodova <b>63,34</b><br/><b>Fizioterapeutski tehničar</b> - min broj bodova <b>68,11</b><br/><b>Dentalni asistent</b> - min broj bodova <b>63,79</b><br/><b>Sanitarni tehničar</b> - min broj bodova <b>59,45</b>";
 
-let turistickoUgostiteljskaJS = "<b>Hoteljersko-turistički tehničar</b> - min broj bodova <b>66,87</b><br/><b>Turističko-hoteljerski komercijalist</b> - min broj bodova <b>64,23</b><br/><b>Konobar</b> - min broj bodova <b>22,59</b><br/><b>Kuhar</b> - min broj bodova <b>26,57</b><br/><b>Slastičar</b> - min broj bodova <b>34,29</b><br/><b>Pomoćni konobar - TES</b><br/><b>Pomoćni kuhar i slastičar - TES</b>";
+let komercijalnoTrgovackaJS =
+  "<b><mark>Komercijalno-Trgovačka škola</mark></b><br/><b>Komercijalist</b> - min broj bodova <b>49,90</b><br/><b>Prodavač</b> - min broj bodova <b>23,07</b>";
 
+let tehnickaPrometnaJS =
+  "<b><mark>Tehnička Prometna škola</mark></b><br/><b>Tehničar cestovnog prometa</b> - min broj bodova <b>40,27</b><br/><b>Tehničar za cestovne i financijske usluge</b> - min broj bodova <b>39,46</b><br/><b>Zrakoplovni prometnik</b> - min broj bodova <b>42,58</b><br/><b>Vozać motornog vozila</b> - min broj bodova <b>22,42</b>";
 
+let pomorskaJS =
+  "<b><mark>Pomorska škola</mark></b><br/><b>Pomorski nautičar</b> - min broj bodova <b>54,95</b><br/><b>Ribarsko-nautički tehničar</b> - min broj bodova <b>41,16</b><br/><b>Tehničar za logistiku i špediciju</b> - min broj bodova <b>45,24</b><br/><b>Tehničar za brodostrojarstvo</b> - min broj bodova <b>49,07</b>";
+
+let prirodoslovnoTehnickaJS =
+  "<b><mark>Prirodoslovno-Tehnička škola</mark></b><br/><b>Prirodoslovna gimnazija</b> - min broj bodova <b>74,77</b><br/><b>Ekološki tehničar</b> - min broj bodova <b>48,72</b><br/><b>Kemijski tehničar</b> - min broj bodova <b>45,62</b><br/><b>Prehranbeni tehničar</b> - min broj bodova <b>50,11</b>";
+
+let graditeljskoGeodetskaTehnickaJS =
+  "<b><mark>Graditeljsko-Geodetska Tehnička škola</mark></b><br/><b>Arhitektonski tehničar</b> - min broj bodova <b>66,00</b><br/><b>Građevinski tehničar</b> - min broj bodova <b>60,18</b><br/><b>Tehničar geodezije i geoinformatike</b> - min broj bodova <b>66,95</b>";
+
+let obrtnickaJS =
+  "<b><mark>Obrtnička škola</mark></b><br/><b>Modni tehničar</b> - min broj bodova <b>46,15</b><br/><b>Kozmetičar</b> - min broj bodova <b>60,42</b><br/><b>Tehničar za očnu optiku</b> - min broj bodova <b>55,19</b><br/><b>Fotograf JMO</b> - min broj bodova <b>26,47</b><br/><b>Pediker JMO</b> - min broj bodova <b>26,17</b><br/><b>Frizer JMO</b> - min broj bodova <b>55,19</b><br/><b>Krojać JMO</b> - min broj bodova <b>24,53</b><br/><b>Galanterist</b> - min broj bodova <b>19,16</b><br/><b>Fotograf JMO</b> - min broj bodova <b>27,20</b><br/><b>Krojač</b> - min broj bodova <b>24,53</b>";
+
+let turistickoUgostiteljskaJS =
+  "<b><mark>Turističko-Ugostoteljska škola</mark></b><br/><b>Hoteljersko-turistički tehničar</b> - min broj bodova <b>66,87</b><br/><b>Turističko-hotelski komercijalist</b> - min broj bodova <b>64,23</b><br/><b>Konobar</b> - min broj bodova <b>22,59</b><br/><b>Kuhar</b> - min broj bodova <b>26,57</b><br/><b>Slastičar</b> - min broj bodova <b>34,29</b><br/><b>Pomoćni konobar TES</b> - min broj bodova <b>--</b><br/><b>Pomoćni kuhar i slastičar-TES</b> - min broj bodova <b>--</b>";
+
+let obrtnaTehnickaJS =
+  "<b><mark>Obrtna Tehnička škola</mark></b><br/><b>Drvodjelni tehničar dizajner</b> - min broj bodova <b>45,28</b><br/><b>Tehničar za vozila i vozna sredstva</b> - min broj bodova <b>40,59</b><br/><b>Tehničar za električnr strojeve s primjenjenim računalstvom</b> - min broj bodova <b>51,06</b><br/><b>Autoelektričar JMO</b> - min broj bodova <b>28,27</b><br/><b>Instalater grijanja i klimatizacije JMO</b> - min broj bodova <b>26,79</b><br/><b>Autolimar JMO</b> - min broj bodova <b>22,72</b><b><br/>Automehaničar JMO</b> - min broj bodova <b>25,45</b><br/><b>Vodoinstalater JMO</b> - min broj bodova <b>28,47</b><b><br/>Elektroinstalater JMO</b> - min broj bodova <b>26,36</b><br/><b>Elektromehaničar JMO</b> - min broj bodova <b>23,19</b><br/><b>Stolar JMO</b> - min broj bodova <b>24,95</b>";
+
+let dizajnGrafikaGradnja1JS =
+  "<b><mark>Škola za dizajn, grafiku i održivu gradnju</mark></b><br/><b>Likovnaa umjetnost i dizajn do izbora zanimanja</b> - min broj bodova <b>145,39</b><br/><b>Tehničar održivog razvoja i gradnje</b> - min broj bodova <b>--</b><br/><b>Keramičar oblagač</b> - min broj bodova <b>23,44</b><br/><b>Monter suhe gradnje</b> - min broj bodova <b>22,23</b><br/><b>Rukovatelj samohodnim građevinskim strojevima</b> - min broj bodova <b>23,09</b><br/><b>Autolakirer JMO</b> - min broj bodova <b>22,33</b><br/><b>Soboslikar-ličilac JMO</b> - min broj bodova <b>22,08</b><br/><b>Zidar, tesar, fasader JMO</b> - min broj bodova <b>23,43</b><br/><b>Grafički tehničar</b> - min broj bodova <b>51,76</b><br/><b>Grafički tehničar pripreme</b> - min broj bodova <b>54,46</b><br/><b>Grafički urednik-dizajner</b> - min broj bodova <b>61,84</b><br/><b>WEB dizajner</b> - min broj bodova <b>65,95</b><br/><b>Medijski tehničar</b> - min broj bodova <b>56,80</b><br/>";
+
+let dizajnGrafikaGradnja2JS =
+  "<b>Web programer</b> - min broj bodova <b>61,91</b><br/><b>Grafičar dorade</b> - min broj bodova <b>27,04</b><br/><b>Grafičar pripreme</b> - <small>min broj bodova</small> <b>22,99</b><br/><b>Pomoćni kartonažer - TES</b> - <small>min broj bodova</small> <b>--</b>";
+
+let dizajnGrafikaGradnjaJS = dizajnGrafikaGradnja1JS + dizajnGrafikaGradnja2JS;
+
+let industriskaJS =
+  "<b><mark>Industrijska škola</mark></b><br/><b>Brodomehaničar IG</b> - <small>min broj bodova</small> <b>22,02</b><br/><b>Instalater-monter IG</b> - <small>min broj bodova</small> <b>24,18</b><br/><b>CNC operater IG</b> - <small>min broj bodova</small> <b>26,41</b><br/><b>Elektromehaničar IG</b> - <small>min broj bodova</small> <b>24,15</b>";
+
+let likovnihUmjetnostiJS =
+  "<b><mark>Škola Likovnih Umjetnosti</mark></b><br/><b>Likovna umjetnost i dizajn</b> - <small>min broj bodova</small> <b>138,83</b>";
+
+let glazbenaJosipaHatzeaJS =
+  "<b><mark>Glazbena škola Josipa Hatzea</mark></b><br/><b>Glazbenik - program srednje škole</b> - <small>min broj bodova</small> <b>--</b><br/><b>Glazbenik - pripremno obrazovanje</b> - <small>min broj bodova</small> <b>--</b>";
+
+let bracaRadicJS =
+  "<b><mark>Braća Radić Kaštel Štafilić-Nehaj</mark></b><br/><b>Opća gimnazija</b> - <small>min broj bodova</small> <b>62,97</b><br/><b>Jezična gimnazija</b> - <small>min broj bodova</small> <b>37,52</b><br/><b>Agroturistički tehničar</b> - <small>min broj bodova</small> <b>--</b><br/><b>Agrotehničar</b> - <small>min broj bodova</small> <b>--</b><br/><b>Tehničar nutricionist</b> - <small>min broj bodova</small> <b>--</b><br/><b>Prehrambeni tehničar</b> - <small>min broj bodova</small> <b>--</b><br/><b>Veterinarski tehničar</b> - <small>min broj bodova</small> <b>--</b><br/><b>Cvječar IG</b> - <small>min broj bodova</small> <b>--</b><br/><b>Pekar</b> - <small>min broj bodova</small> <b>--</b><br/><b>Mesar</b> - <small>min broj bodova</small> <b>--</b><br/><b>Pomoćni cvječar - TES</b> - <small>min broj bodova</small> <b>--</b>";
+
+let wallnerJS =
+  "<b><mark>Privatna srednja ugostiteljska škola Wallner</mark></b><br/><b>Turističko-hoteljerski komercijalist</b> - <small>min broj bodova</small> <b>42,35</b><br/><b>Hoteljersko-turistički tehničar</b> - <small>min broj bodova</small> <b>37,52</b><br/><b>Konobar JMO</b> - <small>min broj bodova</small> <b>--</b><br/><b>Kuhar JMO</b> - <small>min broj bodova</small> <b>22,83</b><br/><b>Slastičar JMO</b> - <small>min broj bodova</small> <b>31,75</b>";
+
+let pitagoraJS =
+  "<b><mark>Privatna jezična gimnazija Pitagora</mark></b><br/><b>Jezična gimnazija</b> - <small>min broj bodova</small> <b>--</b><br/><b>Tehničar za računarstvo</b> - <small>min broj bodova</small> <b>--</b>";
+
+let dentalCentarMarusicJS =
+  "<b><mark>Srednja škola dental centar Marušić</mark></b><br/><b>Dentalni tehničar</b> - <small>min broj bodova</small> <b>--</b><br/><b>Dentalni asistent</b> - <small>min broj bodova</small> <b>--</b><br/><b>Fizioterapeutski tehničar</b> - <small>min broj bodova</small> <b>--</b>";
+
+let markoAntunDominisJS =
+  "<b><mark>Privatna srednja škola Marko Antun Dominis</mark></b><br/><b>Opća gimnazija</b> - <small>min broj bodova</small> <b>43,55</b><br/><b>Ekonomist</b> - <small>min broj bodova</small> <b>44,35</b><br/><b>Hoteljersko-turistički tehničar</b> - <small>min broj bodova</small> <b>38,67</b>";
+
+let kraljicaJelenaJS =
+  "<b><mark>Gimnazijski kolegij Kraljica Jelena</mark></b><br/><b>Gimnazijski program - eksperiment</b> - <small>min broj bodova</small> <b>43,11</b>";
 
 //! ************************************************************
 
-
 document.getElementById("strojarska").addEventListener("click", function () {
-    document.getElementById("nesto").innerHTML = strojarstvo;
+  document.getElementById("nesto").innerHTML = strojarstvo;
 });
 
 document.getElementById("elektro").addEventListener("click", function () {
-    document.getElementById("nesto").innerHTML = tehnicar;
+  document.getElementById("nesto").innerHTML = elektroJS;
 });
 
 document.getElementById("gimnazijaPrva").addEventListener("click", function () {
-    document.getElementById("nesto").innerHTML = gimnazija1;
+  document.getElementById("nesto").innerHTML = gimnazija1;
 });
 
 document.getElementById("gimnazijaDruga").addEventListener("click", function () {
-    document.getElementById("nesto").innerHTML = gimnazija2;
+  document.getElementById("nesto").innerHTML = gimnazija2;
 });
 
 document.getElementById("gimnazijaTreca").addEventListener("click", function () {
-    document.getElementById("nesto").innerHTML = gimnazija3;
+  document.getElementById("nesto").innerHTML = gimnazija3;
 });
 
-document.getElementById("gimnazijaCetvrta").addEventListener("click", function () {
+document
+  .getElementById("gimnazijaCetvrta")
+  .addEventListener("click", function () {
     document.getElementById("nesto").innerHTML = gimnazija4;
-});
+  });
 
 document.getElementById("gimnazijaPeta").addEventListener("click", function () {
-    document.getElementById("nesto").innerHTML = gimnazija5;
+  document.getElementById("nesto").innerHTML = gimnazija5;
 });
 
-document.getElementById("ekonomskoBirotehnicka").addEventListener("click", function () {
+document
+  .getElementById("ekonomskoBirotehnicka")
+  .addEventListener("click", function () {
     document.getElementById("nesto").innerHTML = ekonomskoBirotehnickaJS;
-});
+  });
 
-document.getElementById("zdravstvenaSkola").addEventListener("click", function () {
+document
+  .getElementById("zdravstvenaSkola")
+  .addEventListener("click", function () {
     document.getElementById("nesto").innerHTML = zdravstvenaJS;
+  });
+
+document
+  .getElementById("turistickoUgostiteljska")
+  .addEventListener("click", function () {
+    document.getElementById("nesto").innerHTML = turistickoUgostiteljskaJS;
+  });
+
+document
+  .getElementById("tehnickaPrometna")
+  .addEventListener("click", function () {
+    document.getElementById("nesto").innerHTML = tehnickaPrometnaJS;
+  });
+
+document.getElementById("pomorska").addEventListener("click", function () {
+  document.getElementById("nesto").innerHTML = pomorskaJS;
 });
 
-document.getElementById("turistickoUgostiteljska").addEventListener("click", function () {
-    document.getElementById("nesto").innerHTML = turistickoUgostiteljskaJS;
+document
+  .getElementById("prirodoslovnoTehnicka")
+  .addEventListener("click", function () {
+    document.getElementById("nesto").innerHTML = prirodoslovnoTehnickaJS;
+  });
+
+document
+  .getElementById("graditeljskoGeodetskaTehnicka")
+  .addEventListener("click", function () {
+    document.getElementById(
+      "nesto"
+    ).innerHTML = graditeljskoGeodetskaTehnickaJS;
+  });
+
+document.getElementById("obrtnicka").addEventListener("click", function () {
+  document.getElementById("nesto").innerHTML = obrtnickaJS;
+});
+
+document.getElementById("obrtnaTehnicka").addEventListener("click", function () {
+  document.getElementById("nesto").innerHTML = obrtnaTehnickaJS;
+});
+
+document
+  .getElementById("dizajnGrafikaGradnja")
+  .addEventListener("click", function () {
+    document.getElementById("nesto").innerHTML = dizajnGrafikaGradnjaJS;
+  });
+
+document.getElementById("industriska").addEventListener("click", function () {
+  document.getElementById("nesto").innerHTML = industriskaJS;
+});
+
+document
+  .getElementById("likovnihUmjetnosti")
+  .addEventListener("click", function () {
+    document.getElementById("nesto").innerHTML = likovnihUmjetnostiJS;
+  });
+
+document
+  .getElementById("glazbenaJosipaHatzea")
+  .addEventListener("click", function () {
+    document.getElementById("nesto").innerHTML = glazbenaJosipaHatzeaJS;
+  });
+
+document.getElementById("bracaRadic").addEventListener("click", function () {
+  document.getElementById("nesto").innerHTML = bracaRadicJS;
+});
+
+document.getElementById("wallner").addEventListener("click", function () {
+  document.getElementById("nesto").innerHTML = wallnerJS;
+});
+
+document
+  .getElementById("dentalCentarMarusic")
+  .addEventListener("click", function () {
+    document.getElementById("nesto").innerHTML = dentalCentarMarusicJS;
+  });
+
+document.getElementById("pitagora").addEventListener("click", function () {
+  document.getElementById("nesto").innerHTML = pitagoraJS;
+});
+
+document
+  .getElementById("markoAntunDominis")
+  .addEventListener("click", function () {
+    document.getElementById("nesto").innerHTML = markoAntunDominisJS;
+  });
+
+document.getElementById("kraljicaJelena").addEventListener("click", function () {
+  document.getElementById("nesto").innerHTML = kraljicaJelenaJS;
 });
 
 //! ************************************************************
